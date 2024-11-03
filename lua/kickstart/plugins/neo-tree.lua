d4feed16a61daebc9637f30bdab44d2c0fa42063
Jesse Schneider <jessewschneider96@gmail.com>
2024-11-03 15:09:27 +1000

    feat: started breaking plugins out into files

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -1,25 +1,36 @@
 -- Neo-tree is a Neovim plugin to browse the file system
 -- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
-  'nvim-neo-tree/neo-tree.nvim',
-  version = '*',
-  dependencies = {
-    'nvim-lua/plenary.nvim',
-    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-    'MunifTanjim/nui.nvim',
-  },
-  cmd = 'Neotree',
-  keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
-  },
-  opts = {
-    filesystem = {
-      window = {
-        mappings = {
-          ['\\'] = 'close_window',
-        },
-      },
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v2.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
     },
+    config = function()
+      local neotree = require 'neo-tree'
+      neotree.setup {
+        close_if_last_window = true,
+        name = {
+          trailing_slash = true,
+          use_git_status_colors = true,
+          highlight = 'NeoTreeFileName',
+        },
+        window = {
+          width = 50,
+          mappings = {
+            ['l'] = 'open',
+            ['h'] = 'close_node',
+          },
+        },
+        filesystem = {
+          follow_current_file = true,
+        },
+      }
+
+      -- keymaps
+      vim.keymap.set('n', '<leader>e', '<cmd>Neotree filesystem reveal float<CR>')
+    end,
   },
 }
